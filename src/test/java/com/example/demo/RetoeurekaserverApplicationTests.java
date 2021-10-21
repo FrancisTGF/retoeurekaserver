c18b1f938202b409e480b24511ddd52d9eb785e5
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-21 15:12:40 +0200

    implementado

--- src/test/java/com/example/demo/RetoeurekaserverApplicationTests.java
+++ src/test/java/com/example/demo/RetoeurekaserverApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Retoeurekaclient1ApplicationTests {
+class RetoeurekaserverApplicationTests {
 
 	@Test
 	void contextLoads() {
