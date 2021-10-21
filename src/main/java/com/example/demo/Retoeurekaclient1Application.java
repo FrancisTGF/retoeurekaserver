7b07a7e61aa11f30ac2d8a7b706bbdf46ee66242
Francisco Tomas Guzman Fernandez <francistomas96@gmail.com>
2021-10-21 14:30:28 +0200

    first commit

---
+++ src/main/java/com/example/demo/Retoeurekaclient1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Retoeurekaclient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Retoeurekaclient1Application.class, args);
+	}
+
+}
